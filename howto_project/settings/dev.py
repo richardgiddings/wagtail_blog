ce96b7e0ea35cef5e4c3ddbee5f6a06854a8881d
Richard Giddings <mail@richardgiddings.co.uk>
2017-03-13 14:38:01 +0000

    Add extra file

---
+++ howto_project/settings/dev.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!ln0=bd(fyls_iglj7n((9==s3i9h=p#44+bl5inlbiww-95ht'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
